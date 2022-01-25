afa471dea505d6cfdefea060f7a2b9a68a36a0a2
Avraham Appel <avraham.appel@humi.ca>
2022-01-25 12:26:11 -0500

    Pair combinator

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ struct Element {
     children: Vec<Element>,
 }
 
-fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
+pub fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
     move |input| -> Result<(&str, ()), &str> {
         match input.get(..expected.len()) {
             Some(next) if next == expected => Ok((&input[expected.len()..], ())),
@@ -13,7 +13,7 @@ fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), 
     }
 }
 
-fn identifier(input: &str) -> Result<(&str, String), &str> {
+pub fn identifier(input: &str) -> Result<(&str, String), &str> {
     let mut matched = String::new();
     let mut chars = input.chars();
 
@@ -30,6 +30,23 @@ fn identifier(input: &str) -> Result<(&str, String), &str> {
     }
 
     Ok((&input[matched.len()..], matched))
+}
+
+pub fn pair<P1, P2, R1, R2>(
+    parser1: P1,
+    parser2: P2,
+) -> impl Fn(&str) -> Result<(&str, (R1, R2)), &str>
+where
+    P1: Fn(&str) -> Result<(&str, R1), &str>,
+    P2: Fn(&str) -> Result<(&str, R2), &str>,
+{
+    move |input| match parser1(input) {
+        Ok((next_input, result1)) => match parser2(next_input) {
+            Ok((final_input, result2)) => Ok((final_input, (result1, result2))),
+            Err(err) => Err(err),
+        },
+        Err(err) => Err(err),
+    }
 }
 
 #[test]
@@ -57,4 +74,15 @@ fn identifier_parser() {
         Err("!not at all an identifier"),
         identifier("!not at all an identifier")
     );
+}
+
+#[test]
+fn pair_combinator() {
+    let tag_opener = pair(match_literal("<"), identifier);
+    assert_eq!(
+        Ok(("/>", ((), "my-first-element".to_string()))),
+        tag_opener("<my-first-element/>")
+    );
+    assert_eq!(Err("oops"), tag_opener("oops"));
+    assert_eq!(Err("!oops"), tag_opener("<!oops"));
 }
