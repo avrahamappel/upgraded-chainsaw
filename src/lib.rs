adef895a1dde65ca2a2bb6474aa438ccb866906c
Avraham Appel <avraham.appel@humi.ca>
2022-01-25 12:13:56 -0500

    Identifier parser

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,30 @@ struct Element {
 
 fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
     move |input| -> Result<(&str, ()), &str> {
-        match input.get(0..expected.len()) {
+        match input.get(..expected.len()) {
             Some(next) if next == expected => Ok((&input[expected.len()..], ())),
             _ => Err(input),
         }
     }
+}
+
+fn identifier(input: &str) -> Result<(&str, String), &str> {
+    let mut matched = String::new();
+    let mut chars = input.chars();
+
+    match chars.next() {
+        Some(next) if next.is_alphabetic() => matched.push(next),
+        _ => return Err(input),
+    }
+
+    while let Some(next) = chars.next() {
+        if !(next.is_alphanumeric() || next == '-') {
+            break;
+        }
+        matched.push(next);
+    }
+
+    Ok((&input[matched.len()..], matched))
 }
 
 #[test]
@@ -22,4 +41,20 @@ fn literal_parser() {
         parse_joe("Hello Joe! Hello Robert!")
     );
     assert_eq!(Err("Hello Mike!"), parse_joe("Hello Mike!"));
+}
+
+#[test]
+fn identifier_parser() {
+    assert_eq!(
+        Ok(("", "i-am-an-identifier".to_string())),
+        identifier("i-am-an-identifier")
+    );
+    assert_eq!(
+        Ok((" entirely an identifier", "not".to_string())),
+        identifier("not entirely an identifier")
+    );
+    assert_eq!(
+        Err("!not at all an identifier"),
+        identifier("!not at all an identifier")
+    );
 }
